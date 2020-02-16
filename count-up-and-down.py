56235e3b8b6f0e2d03703695e63e338704efd506
bodhibrdr <julie.gumerman@gmail.com>
2020-02-16 12:49:02 -0700

    fixed and expanded a function

--- count-up-and-down.py
+++ count-up-and-down.py
@@ -1,8 +1,11 @@
 def countUp(num):
-    count = 0
+    count = 1
     while count <= num:
         print(f" Number: {count}")
         count += 1
+    while count >= 2:
+        count -= 1
+        print(f"Number: {count}")
 
     print("And Done!!!")
 
